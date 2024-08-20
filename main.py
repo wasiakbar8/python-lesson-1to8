279cd2ec5a39a2d9b81a480bc8c197ff0b329523
wasi <syedwasiakbar@gmail.com>
2024-08-20 10:18:52 +0500

    code with harrry lession 1 to 8

---
+++ main.py
@@ -0,0 +1,19 @@
+#comment
+#this is the way to write a comment. now we can use eascape sequence in string.
+
+print("my name is wasi \n and i am a student of BSCs")
+ #now we are gonig to complete our first exercise .
+ #we have 2 numbers and we have to make a simple arthimetic calculator.
+ 
+a = 55
+b =45
+# Here 'a' and 'b' are the datatypes whic are holding some values.
+print("we have the numbers 55 and 45")
+ 
+print("the addition of a+b =",a+b)
+print("the subtraction of a-b =",a-b)
+print("the product of a+b =",a*b)
+print("the division of a/b =",a/b)
+
+# now we are goint to fint out the type of our datatypes
+print("the type of a is",type(a))
