9df1e95aa07fe79d0471332e57f705e14bacf86d
wasi <syedwasiakbar@gmail.com>
2024-08-21 10:33:20 +0500

    this is typecasting program

---
+++ main2.py
@@ -0,0 +1,13 @@
+#typecasting 
+#we can convert one datatype into another datatype:
+
+a="5"
+b="5"
+#befor typecasting  the out put is:
+print(a+b)
+#the output is 55:
+
+#now the output is after typecasting:
+print(int(a)+int(b)) 
+#now the output is 10; so this is a typecasting
+
