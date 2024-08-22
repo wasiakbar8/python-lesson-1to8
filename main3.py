dacfe91c6122f405d1d34fa418f4218e60c1aa48
wasi <syedwasiakbar@gmail.com>
2024-08-22 09:07:36 +0500

    getting user value in python

---
+++ main3.py
@@ -0,0 +1,9 @@
+# Get the two numbers from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Add the two numbers together
+result = num1 + num2
+
+# Print the result
+print("The sum of the two numbers is:", result)
